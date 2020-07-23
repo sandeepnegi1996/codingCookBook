ea90de5b3436007425de9085fc10a6c0b548c210
sandeep <negi.sandeepsingh@bosch.com>
2020-07-23 16:28:40 +0530

    created the basic structure for jnuit tests

---
+++ java/junitcourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,27 @@
+package com.in28minutes.junit.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("before test");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("after test");
+	}
+
+	@Test
+	public void test() {
+
+
+	}
+
+}
