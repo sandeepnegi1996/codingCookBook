51684305edaa5578bd483978d2b3f7741d834b2e
sandeep <negi.sandeepsingh@bosch.com>
2020-08-25 09:40:27 +0530

    learned about the scopes of the variables

---
+++ java/Book_Java_OCP_11_SE_1/chapter4/programs/ScopeOfVar.java
@@ -0,0 +1,33 @@
+class Test {
+
+
+	//private String name="sandy";
+	//private static int age=23;
+
+	public static void main(String args[]) {
+
+		String name="negi";
+		int age=45;
+		//System.out.println("This is inside the method :  "+name);
+		//System.out.println("this Age is inside the main :  " +age);
+	
+		
+		int i=1;
+
+		//for(int i=2;i<5;i++) {
+		//	System.out.println("This is inside for loop: "+i);
+
+		//}
+		
+
+		{
+			int i=30;
+
+		}
+
+
+
+
+	}
+
+}
